a5392bd8d1d63b33f8b0d750606d5b956c5589ff
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2024-03-08 18:23:50 +0530

    Add files via upload

---
+++ variable_1.py
@@ -0,0 +1,5 @@
+# The two value to add the formula
+a=20                   # The value store in a
+b=35                   # The value store in n
+c=a+b                  # The formula to add two variable
+print("The sum is",c) 
